973957b25907cb64ba8bc6fd898a1cf3a78f0ec0
Scott Lougheed <scott@scottlougheed.com>
2022-10-11 20:32:29 -0700

    First complete draft

--- op-cli-example-scripts/user-management/suspend-absentees.sh
+++ op-cli-example-scripts/user-management/suspend-absentees.sh
@@ -3,55 +3,25 @@
 # Store output of op only for currently-active users. 
 json=$(op user list --format=json --account michaelscottpapercompany | op user get - --format=json --account michaelscottpapercompany | jq -r 'select(.state == "ACTIVE")') 
 
-# Get the date in seconds 90 days ago
-# date -j -v -90d +"%s" 
-current_date=$(date +"%s")
-
 # Set threshold for max idle time
-# threshold=7776000 #90 days represented as seconds
-threshold=548475 # temporary value for development to ensure mix of results. 
+threshold=548475 # temporary value for development to ensure mix of results. Change to 7776000 for 90 days. 
+
+# Convert threshold in seconds to number of days for use where human readability is required. 
 threshold_days=$(date -j -f "%s" $threshold +"%d")
 
-# calculate the difference between now and last_auth_at
-
+# calculate the difference between now and last_auth_at and select only those users idle for longer than $threshold
 idle_users=$(echo $json | jq --argjson threshold $threshold 'if now - (.last_auth_at | strptime("%Y-%m-%dT%H:%M:%SZ") | mktime) > $threshold then .idle="true" else .idle="false" end | select(.idle == "true")')
 
-echo $idle_users | jq 'length' | wc -l
-
 echo 'The following users have been idle for longer than '$threshold_days' days'
-echo $idle_users | jq -r '.id " " .name'
-# The stuff below here is all just scraps 
 
-# How to get json into a bash array: https://blog.kellybrazil.com/2021/04/12/practical-json-at-the-command-line/
-# Because bash is allergic to json, maybe best to do all the conditional
-# selection in jq. 
+# Print the UUID and name of each idle user
+echo $idle_users | jq -r '.id + "   " + .name' | awk  'BEGIN { FS="\t" } { print $1, $2 }'
 
-# One approach might be to actually do it in two steps: calculate a new idleTime:seconds k:v pair that is appended to the json, 
-# then in a second step, iterate through the json to find objects with idleTime > threshold. 
-
-echo $(op user list --format=json | op user get - --format=json | jq -r 'select(.state == "ACTIVE") | if (.last_auth_at | strptime("%Y-%m-%dT%H:%M:%SZ")) - "'$current_date'" > $threshold then [ADD,THE,UUID,TO,AN,ARRAY?]  ')
-# strptime("%Y-%m-%dT%H:%M:%SZ") | mktime will convert the date provided by 1Password to unix time
-
-echo $json | jq 'if .last_auth_at | strptime("%Y-%m-%dT%H:%M:%SZ") | mktime - todate > env.threshold then .name else "active"'
+# Print only the UUID of each idle user
+echo $idle_users | jq -r '.id + "   " + .name' | awk  '{ print $1 }' 
 
 
+# At this point you could assign those UUID's to an array:
+# idle_user_array=($(echo $idle_users | jq -r '.id + "   " + .name' | awk  '{ print $1 }'))
+# then loop through the array with op suspend $uuid
 
-
-for user in $all_users
-    do
-        user_last_auth=$(echo $user | jq -r '.last_auth_at')
-        echo "user last auth:  $user_last_auth"
-        seconds  date -j -f "%F" echo $user_last_auth | sed -E 's/(T..:..:...)//g' +"%s"
-        echo "seconds:  $seconds"
-    done
-
-
-for user in $all_users
-do
-    echo $user | jq
-done
-
-for user in $all_users 
-do
-    echo $(echo $user | jq -r '[].last_auth_at')
-done
