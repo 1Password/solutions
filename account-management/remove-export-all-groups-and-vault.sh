1e2bcbb0d89ad4818ae1be0ba577acdafdccba0b
Scott Lougheed <scott@scottlougheed.com>
2022-11-11 16:04:53 -0800

    Scoot/remove group permission from all vaults (#4)
    * adding permission removal script
    
    * added comments
    
    * more comments

---
+++ account-management/remove-export-all-groups-and-vault.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# This script removes the Export permission for ALL VAULTS that EVERY group has access to. 
+# This includes the Owners and Administrators groups. 
+# This script requires `jq` is installed on your system. See: https://stedolan.github.io/jq/ for installation instructions.
+
+vault_IDs=($(op vault list --format=json | jq --raw-output '.[] .id'))
+
+for vault in $vault_IDs 
+do
+    echo "outer loop"
+    for group in $(op group list --vault $vault --format=json | jq --raw-output '.[] .id') 
+    do
+        echo "inner loop"
+        op vault group revoke \
+	    --permissions export_items \
+	    --group $group \
+	    --vault $vault
+    done
+done
+
+
