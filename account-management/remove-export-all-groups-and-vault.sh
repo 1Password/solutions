42a97d30ede15489e9bfc4f916413fd01d0f4174
Scott Lougheed <scott@scottlougheed.com>
2022-11-11 16:34:04 -0800

    removing debugging code

--- account-management/remove-export-all-groups-and-vault.sh
+++ account-management/remove-export-all-groups-and-vault.sh
@@ -12,10 +12,8 @@ vault_IDs=($(op vault list --format=json | jq --raw-output '.[] .id'))
 
 for vault in $vault_IDs 
 do
-    echo "outer loop"
     for group in $(op group list --vault $vault --format=json | jq --raw-output '.[] .id') 
     do
-        echo "inner loop"
         op vault group revoke \
 	    --permissions export_items \
 	    --group $group \
