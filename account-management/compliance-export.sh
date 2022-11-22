993fbcc6d065ca7b94cd76b92b35b3e26cd2f6ff
Scott Lougheed <scott@scottlougheed.com>
2022-11-22 15:14:13 -0800

    changed exclusion criteria and fixed selector
    Changed the exclusion criteria to .type != "CONCEALED" rather than .label != "Password". This should accommodate templates with password fields with other names (like "password") or with additional secret fields (like a PIN).

--- account-management/compliance-export.sh
+++ account-management/compliance-export.sh
@@ -8,16 +8,13 @@
 # Replace with the tag to select all items with that tag. 
 # Note that if you specify multiple tags, they are separated by an "or", meaning
 # an item with `tag a` OR `tag b` or both `tag a` and `tag b` will be selected . 
-tag="" 
 
 # This will write output to a file called reports.csv in the same directory as the script. 
 # If you prefer a different output location, change this to the path and filename of your choosing
 outputFile="report.csv"
 
-for item in $(op item list --format=json | jq --raw-output '.[].id')
+for item in $(op item list --tags $tag --format=json | jq --raw-output '.[].id')
 do
-    op item get $item --tags $tag --format json | jq -r '{label: "Item Name", value: .title},{label: "Vault", value: .vault.name},{label: "Date Created", value: .created_at},{label: "Date Updated", value: .updated_at},.fields[] | select(.label != "Password") | [.label, .value] |@csv' >> $outputFile
+    op item get $item --format json | jq -r '{label: "Item Name", value: .title},{label: "Vault", value: .vault.name},{label: "Date Created", value: .created_at},{label: "Date Updated", value: .updated_at},.fields[] | select(.type != "CONCEALED") | [.label, .value] |@csv' >> $outputFile
     echo "" >> $outputFile
 done
-
-
