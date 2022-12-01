3443f15bf435c3215bd9d71e4128193c98b93b1d
Scott Lougheed <scott@scottlougheed.com>
2022-12-01 14:28:31 -0800

    removing confusing comments

--- account-management/bulk-group-prefix-update.sh
+++ account-management/bulk-group-prefix-update.sh
@@ -3,10 +3,7 @@
 # This script will change the prefix on all groups that contain the specified prefix. 
 # Adjust the values as desired on the line indicated by comments below. 
 # This script will not change the name of any built-in groups (e.g., "Administrators" or "Owners")
-# This script will indescriminately match the start of the group name. This means if you have a group
-# Called "ONTARIO" and are replacing the prefix "ON_" for groups like "ON_all staff" and "ON_Human Resources"
-# you may see some onwanted substitutions. Group names are printed before and after transformation so you can identify
-# any mis-transformed names, 
+
 
 groups=$(op group list | awk 'NR>1 { print $1 }')
 for group in $groups
