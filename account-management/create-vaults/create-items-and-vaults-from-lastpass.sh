dafec90a9b9df5df878dcca42391f08238f37b10
Scott Lougheed <scott@scottlougheed.com>
2022-12-29 14:16:26 -0800

    more draft code removed

--- account-management/create-vaults/create-items-and-vaults-from-lastpass.sh
+++ account-management/create-vaults/create-items-and-vaults-from-lastpass.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-
-vault_list=()
 while IFS= read -r line; do
+    
+    
     op vault create $line | tee | grep ID: | sed 's/ID:                   //g' >> created
 done < <(tail -n +2 vaultlist | sort -u) # vaultlist can be substituted with path/to/file if you prefer a different input file. 
