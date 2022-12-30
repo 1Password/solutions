d8c3c6893219d693dd166bf7061b1879aec9c1eb
Scott Lougheed <scott@scottlougheed.com>
2022-12-30 11:13:13 -0800

    removing placeholder

--- migration/lastpass-vault-item-import.py
+++ migration/lastpass-vault-item-import.py
@@ -15,7 +15,7 @@
 
 import csv, os
 vault_list = []
-with open('/Users/scottatwork/code/lpexport2.csv', newline='') as csvfile:
+with open('export.csv', newline='') as csvfile:
     linereader = csv.reader(csvfile, delimiter=',', quotechar='"')
     next(linereader)
     
