bfc106130421f83d0bd821f648c59b6362f17c33
Scott Lougheed <scott@scottlougheed.com>
2023-01-25 09:09:15 -0800

    added qualified language to two MP prompt

--- migration/lpass.py
+++ migration/lpass.py
@@ -5,7 +5,7 @@ import sys
 
 def get_lp_data():
     # Sign user into LastPass
-    lp_username = input("You will be prompted once for your LastPass username and twice for your Master Password.\nThe first prompt signs you into the LastPass CLI, the second prompt approves the export.\nWe don't store these credentials and will only be used for this session.\n\nPlease enter your LastPass username.\n>")
+    lp_username = input("You may be prompted once for your LastPass username and twice for your Master Password.\nThe first prompt signs you into the LastPass CLI, the second prompt approves the export.\nWe don't store these credentials and will only be used for this session.\n\nPlease enter your LastPass username.\n>")
     try:
         subprocess.run(["lpass", "login", lp_username])
     except:
