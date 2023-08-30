10857df9c196d09fa59aac5c834ffdd9f436c879
Scott Lougheed <scott@scottlougheed.com>
2023-08-30 15:23:09 -0700

    introducing friendlier date formatting

--- migration/clean-up-scripts/vault-dedupe-helper.py
+++ migration/clean-up-scripts/vault-dedupe-helper.py
@@ -6,6 +6,7 @@ import os
 import subprocess
 import csv
 import json
+from datetime import datetime
 
 scriptPath = os.path.dirname(__file__)
 outputPath = scriptPath  # Optionally choose an alternative output path here.
@@ -23,6 +24,10 @@ def getVaultUserList(vaultID):
 def getVaultDetails(vaultID):
     return subprocess.run(f"op vault get {vaultID} --format=json", shell=True, check=True, capture_output=True).stdout
 
+def formatDate(date):
+    isoDate = datetime.fromisoformat(date)
+    return f"{isoDate.year}-{isoDate.month}-{isoDate.day} {isoDate.hour}:{isoDate.minute}:{isoDate.second}"
+
 def main():
     vaultList = json.loads(getVaults())
     with open(f"{outputPath}/vaultreport.csv", "w", newline="") as outputFile:
@@ -36,13 +41,15 @@ def main():
             vaultUserList = json.loads(getVaultUserList(vault["id"]))
             vaultDetails = json.loads(getVaultDetails(vault["id"]))
             userCount = len(vaultUserList)
+            dateCreated = formatDate(vaultDetails["created_at"])
+            dateUpdated = formatDate(vaultDetails["updated_at"])
 
             csvWriter.writerow([
                 vault["name"],
                 vault["id"],
                 vaultDetails["items"],
-                vaultDetails["created_at"],
-                vaultDetails["updated_at"],
+                dateCreated,
+                dateUpdated,
                 userCount
             ]) 
 main()
