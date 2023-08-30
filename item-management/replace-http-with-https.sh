4a6727d8109b6a9fea5168491b3fc0c836ffbc4b
Scott Lougheed <scott.lougheed@agilebits.com>
2023-08-30 11:44:06 -0700

    add vault flag to op commands for SA (#39)

--- item-management/replace-http-with-https.sh
+++ item-management/replace-http-with-https.sh
@@ -11,7 +11,7 @@ vaultUUID=""
 # add new url to each item
 for item in $(op item list --vault $vaultUUID --categories Login --format json | jq --raw-output '.[].id')
 do
-	oldURL=$(op item get $item --format=json | jq --raw-output '.urls[].href')
+	oldURL=$(op item get $item --vault $vaultUUID --format=json | jq --raw-output '.urls[].href')
 	newURL=$(echo $oldURL | sed 's/^http:\/\//https:\/\//g')
-	op item edit $item website=$newURL
+	op item edit $item --vault $vaultUUID website=$newURL
 done
