ad05437b666e80baa5adaa38bba187f1af22c0aa
Scott Lougheed <scott@scottlougheed.com>
2023-08-31 15:55:20 -0700

    adding -r option to run script in delete mode

--- migration/clean-up-scripts/add_vault_prefix.py
+++ migration/clean-up-scripts/add_vault_prefix.py
@@ -4,11 +4,13 @@ import json
 import sys
 
 parser = argparse.ArgumentParser("Vault Prefixer", "Provide a path to a list of vault UUIDs.", "Prefixes all vaults passed to the script from the specified file with '!'.")
-parser.add_argument("--file", action="store", dest="filepath", required=True)
+parser.add_argument("--file", action="store", dest="filepath", help="Required. Specify the path to a file containing a linebreak-delimited list of vault UUIDs you'd like to process.", required=True)
+parser.add_argument("-r", action="store_true", dest="deletemode", help="Use this option to delete vaults in the provided list, rather than append `!` to the vault name. USE WITH CAUTION.")
 args = parser.parse_args()
 
 # Prefix each vault in the input list with an exclamation mark to make them easy to locate and assess duplicate status. {}
 def prefixVault():
+    print("Prefixing all listed vaults with '!' for easy sorting and identification.\n\n")
     with open(args.filepath, "r", encoding="utf-8") as vaultList:
         for vault in vaultList:
             vaultID = vault.rstrip()
@@ -25,9 +27,9 @@ def prefixVault():
                 print(f"Encountered an error renaming {vaultName}: ", err)
                 continue
             
-
 # Deletes each vault passed into this script
 def deleteVault():
+    print("Deleting vaults provided to the script.\n\n")
     # TRIPLE CHECK that the script runner understands the implications of their decision
     understands = False
     if understands == False:
@@ -49,13 +51,7 @@ def deleteVault():
                 continue
 
 
-
-prefixVault()
-
-########
-# If you want to delete the vaults contained in the list provided to this script,
-# uncomment the function call below and comment prefixVault()
-# DELETING VAULTS IS PERMANENT AND CANNOT BE UNDONE.
-#######
-
-# deleteVault()
+if args.deletemode is True:
+    deleteVault()
+else:
+    prefixVault()
