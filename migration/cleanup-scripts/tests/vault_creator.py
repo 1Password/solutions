92e454fd698ba159357a4b54523d954056114ffc
Scott Lougheed <scott@scottlougheed.com>
2023-09-07 15:02:41 -0700

    deleting tests

--- migration/cleanup-scripts/tests/vault_creator.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def createVaults(number):
-    i = 0
-    while i < number:
-        subprocess.run(f"op vault create vault{i}", shell=True, check=True, capture_output=True)
-        i += 1
-
-createVaults(4)
