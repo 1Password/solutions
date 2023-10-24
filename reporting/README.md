6879cbedd39bc2a08b1e3f3dec20418daf533868
Scott Lougheed <scott@scottlougheed.com>
2023-10-24 16:34:03 -0700

    adding script and updating readme

--- reporting/README.md
+++ reporting/README.md
@@ -2,23 +2,37 @@
 
 This directory provides a series of scripts demonstrating how to use the CLI to generate summary reports of vault access, contents, and permissions.
 
-## [`user-and-item-list.py`](./user-and-item-list.py)
+## User Access Reports
+The following scripts provide information about people who are directly granted access to vaults. It does not include groups.  
+
+### [`user-and-item-list.py`](./user-and-item-list.py)
 
 - Creates a csv file of each user and item that has access to a list of vaults.
   - use `--file path/to/vaultlist` to provide a list of UUIDs, or use no flag to get a report for all vaults the Owners group has access to.
   - Columns included: "vaultName", "vaultUUD", "userName", "userEmail", "userUUID", "userPermissions", "itemName", "itemUUID"
+  - This does not include users who hace access to vaults as a result of membership in a group granted access to a vault. 
 
-## [`vault-user-access-report.py`](./vault-user-access-report.py)
+### [`vault-user-access-report.py`](./vault-user-access-report.py)
 
 - Creates a csv file listing vaults, every user that has been directly granted access to each vault, and their permissions.
   - Use `--file path/to/vaultlist` to provide a list of UUIDs, or use no flag to get a report for all vaults the Owners group has access to.
   - Columns included: "vaultName", "vaultUUID", "name", "email", "userUUID", "userPermissions"
 - This does NOT show users who have access to vaults as a result of their membership in an assigned group.
 
-## [`vault-user-group-access-report.py`](./vault-user-group-access-report.py)
+
+## User and Group access reports
+The following scripts provide information about vault access for both groups and individual users. Some scripts will decompose groups into their individual members, some may only provide information about the group itself.
+
+### [`vault-user-group-access-report.py`](./vault-user-group-access-report.py)
 
 - Creates a csv file listing vaults, every user that has been granted access to the vault directly or by virtue of their group membership.
   - Columns included: "vaultName", "vaultUUID", "userName", "userEmail", "userUUID", "userState", "assignment"
     - "userState" indicates if the user is `ACTIVE`, `SUSPENDED`, `INVITED`, etc. a
     - "assignment" indicates whether they were directly assigned ("direct") or have access to a vault due to membership in an assigned group ("group(groupName)")
   - If users have access to a vault by multiple assignemtns (e.g., is directly assigned and a member of one or more group, or is a member of multiple groups all of which are assigned to the vault) they will appear on one row for every way they've been granted access. 
+
+### [`vault-user-group-permissions-report.py`](./vault-user-group-permissions-report.py)
+- Creates a csv file listing vaults, every active user and group that has been granted access to the vault directly, along with the user or group's permissions.
+  - Columns included: "vaultName",	"vaultUUID",	"userName",	"userEmail",	"userUUID",	"groupName",	"groupUUID",	"permissions"
+- This script does not include suspended or invited users. Only users in the Active state are included. 
+- This script does not decompose groups into their individual members. 
---
+++ reporting/vault-user-group-permissions-report.py
@@ -0,0 +1,176 @@
+# A script that provides a list of users who have access to each vault in
+# a 1Password account, and whether that assignment is a direct assignment or
+# granted by group membership
+#
+# This script must be run by a member of the Owners group of a 1Password Business account
+import os
+import subprocess
+import csv
+import json
+import sys
+from dataclasses import dataclass
+
+scriptPath = os.path.dirname(__file__)
+outputPath = scriptPath
+
+
+class User:
+    users = []
+
+    def __init__(self, name, email, uuid, state, groups=None):
+        self.name = name
+        self.email = email
+        self.uuid = uuid
+        self.state = state
+        self.groups = []
+        self.vaults = []
+
+
+class Group:
+    groups = []
+
+    def __init__(self, name, uuid):
+        self.name = name
+        self.uuid = uuid
+        self.users = []
+
+
+class Vault:
+    vaults = []
+
+    def __init__(self, name, uuid, users=None, groups=None):
+        self.name = name
+        self.uuid = uuid
+        self.users = []
+        self.groups = []
+        Vault.vaults.append(self)
+
+    def addUser(self, user):
+        self.users.append(user)
+
+    @classmethod
+    def getAll(cls):
+        return [inst for inst in cls.vaults]
+
+    @classmethod
+    def getByID(cls, vaultID):
+        for vault in cls.vaults:
+            if vault.uuid == vaultID:
+                return vault
+
+
+def getAllOwnerVaults():
+    vaultList = subprocess.run(
+        ["op", "vault", "list", "--group=Owners", "--format=json"], check=True, capture_output=True).stdout
+    for vault in json.loads(vaultList):
+        Vault(name=vault['name'], uuid=vault['id'],)
+
+
+def getAllUsers():
+    accountUserList = subprocess.run(
+        ["op", "user", "list", "--format=json"], check=True, capture_output=True).stdout
+    for user in json.loads(accountUserList):
+        User(email=user['email'], name=user['name'],
+             uuid=user['id'], state=user['state'])
+
+
+def getAllGroups():
+    accountGroupList = subprocess.run(
+        ["op", "group", "list", "--format=json"], check=True, capture_output=True).stdout
+    for group in json.loads(accountGroupList):
+        Group(name=group['name'], uuid=group['id'])
+
+
+def getVaultUserList(vaultID):
+    vaultUserList = subprocess.run(
+        ["op", "vault", "user", "list", vaultID, "--format=json"], check=True, capture_output=True).stdout
+    return vaultUserList
+
+
+def getVaultGroupList(vaultID):
+    vaultGroupList = subprocess.run(
+        ["op", "vault", "group", "list", vaultID, "--format=json"], check=True, capture_output=True).stdout
+    return vaultGroupList
+
+
+def getGroupMembers(groupID):
+    try:
+        groupMembers = subprocess.run(
+            ["op", "group", "user", "list", groupID, "--format=json"], check=True, capture_output=True).stdout
+    # If the vault has no assigned groups, prevent the script from stopping when None is returned
+    except (Exception):
+        groupMembers = []
+        print("group has no members")
+
+    return groupMembers
+
+
+def writeReport(vaults: Vault):
+
+    with open(f"{outputPath}/vaultAccessReport.csv", "w", newline="") as outputFile:
+        csvWriter = csv.writer(outputFile)
+        fields = ["vaultName", "vaultUUID", "userName",
+                  "userEmail", "userUUID", "groupName", "groupUUID", "permissions"]
+        csvWriter.writerow(fields)
+        for vault in vaults:
+            vaultName = vault.name
+            vaultUUID = vault.uuid
+            # write each row
+            for user in vault.users:
+                if user['assignment'] == "Direct" and user['state'] == "ACTIVE":
+                    csvWriter.writerow(
+                        [vaultName, vaultUUID, user['name'], user['email'], user['uuid'], None, None, user['permissions']])
+            for group in vault.groups:
+                csvWriter.writerow(
+                    [vaultName, vaultUUID, None, None, None, group['name'], group['uuid'], group['permissions']])
+
+
+def main():
+    counter = 1
+    # Populate initial data
+    try:
+        getAllOwnerVaults()
+    except (Exception) as e:
+        sys.exit("Unable to get a list of vaults for the Owner group. Please sign into 1Password as a member of the Owners group. Error: ", e)
+
+    # Get user assignments and group assignments
+    vaults = Vault.getAll()
+    vaultCount = len(vaults)
+    for vault in vaults[:8]:
+        print(
+            f"\tPROCESSING vault {counter}/{vaultCount} \"{vault.name}\". This may take a moment...")
+        try:
+            users = json.loads(getVaultUserList(vault.uuid))
+        except (Exception) as e:
+            sys.exit(
+                f"Unable to get a list of users directly assigned to vault {vault.name}. Error: {e}")
+        for user in users:
+            vault.users.append(
+                {'name': user['name'], 'email': user['email'], 'uuid': user['id'], 'assignment': 'Direct', 'state': user['state'], 'permissions': user['permissions']})
+
+    # For assigned groups, decompose into individual users
+        try:
+            groups = json.loads(getVaultGroupList(vault.uuid))
+        except (Exception) as e:
+            sys.exit(
+                f"Unable to get a list of groups assigned to vault {vault.name}. Error: {e}")
+        for group in groups:
+            vault.groups.append(
+                {'name': group['name'], 'uuid': group['id'], 'permissions': group['permissions']})
+            try:
+                groupUsers = json.loads(getGroupMembers(group['id']))
+            except (Exception) as e:
+                sys.exit(
+                    f"Unable to get a list of group members in group {group['name']} assigned to vault {vault.name}. Error: {e}")
+            if groupUsers is not None:
+                for groupUser in groupUsers:
+                    vault.users.append(
+                        {'name': groupUser['name'], 'email': groupUser['email'], 'uuid': groupUser['id'], 'assignment': f'Group ({group["name"]})', 'state': groupUser['state']})
+        counter += 1
+    try:
+        writeReport(vaults)
+    except (Exception) as e:
+        sys.exit("Unable to write data to a file on disk.", e)
+
+
+main()
