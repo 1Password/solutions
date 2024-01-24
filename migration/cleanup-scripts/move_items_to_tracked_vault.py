b9e2164257a8b8e44b47145c0311a44b71135a24
Scott Lougheed <scott.lougheed@agilebits.com>
2024-01-23 16:56:35 -0800

    implementing initial scaffolding

---
+++ migration/cleanup-scripts/move_items_to_tracked_vault.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+import csv
+import json
+from collections import defaultdict
+from datetime import datetime
+
+scriptPath = os.path.dirname(__file__)
+outputPath = scriptPath  # Optionally choose an alternative output path here.
+
+
+# Get a list of vaults the logged-in user has access to
+def getVaults():
+    try:
+        return subprocess.run(
+            "op vault list --group=Owners --format=json",
+            shell=True,
+            check=True,
+            capture_output=True,
+        ).stdout
+    except Exception as err:
+        print(
+            f"Encountered an error getting the list of vaults you have access to: ", err
+        )
+        return
+
+
+# Get the details of a vault
+def getVaultDetails(vaultID):
+    try:
+        return subprocess.run(
+            ["op", "vault", "get", f"{vaultID}", "--format=json"],
+            check=True,
+            capture_output=True,
+        ).stdout
+    except Exception as err:
+        print(f"Encountered an error getting details for vault {vaultID}: ", err)
+        return
+
+
+def main():
+    vaults = json.loads(getVaults())
+
+
+main()
